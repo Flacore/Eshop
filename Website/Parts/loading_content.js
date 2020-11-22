e2ba58f3305dbab079957dff40eac68ad06ce25a
Michal Molitoris <molitoris2@stud.uniza.sk>
2020-11-22 18:13:19 +0100

    *item list finish {without filter} *shoping cart {only 1pc from item}

--- Website/Parts/loading_content.js
+++ Website/Parts/loading_content.js
@@ -5,9 +5,25 @@ $(document).ready( function() {
     $("#home").on("click", function() {
         $("#content").load("Parts/home.html");
     });
+    $("#shopingCart").on("click", function() {
+        $("#content").load("Parts/shoping_cart.html");
+    });
 });
 
 
 function load_home() {
     $("#content").load("Parts/home.html");
+}
+
+function showList() {
+    var node = document.getElementById('subcategoryList')
+    var text = document.getElementById('showListText')
+    var visibility = node.style.display;
+    if(visibility == "inline"){
+        node.style.display = 'none';
+        text.innerText='Zobrazit podkategorie ⇓';
+    }else{
+        node.style.display = 'inline';
+        text.innerText='Zobrazit podkategorie ⇑';
+    }
 }
--- Website/index.php
+++ Website/index.php
@@ -19,13 +19,11 @@
             <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Kategorie<span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li><a href="#" id="category" >Kategoria 1</a></li>
-                    <li><a href="#" id="category">Kategoria 2</a></li>
-                    <li><a href="#" id="category">Kategoria 3</a></li>
                 </ul>
             </li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Košík</a></li>
+            <li><a href="#"  id="shopingCart"><span class="glyphicon glyphicon-shopping-cart"></span> Košík</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registracia</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Prihlasenie</a></li>
         </ul>
