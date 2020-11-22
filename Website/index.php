2dcd3977cf3ac44a35992947d74be1c098fab0c6
Michal Molitoris <molitoris2@stud.uniza.sk>
2020-11-22 03:10:39 +0100

    item list 20%

--- Website/index.php
+++ Website/index.php
@@ -4,13 +4,13 @@
     <title>eshop</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="CSS/design.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="CSS/design.css">
     <script src="Parts/loading_content.js"></script>
 </head>
-<body>
+<body onload="load_home()">
 
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
@@ -32,11 +32,7 @@
     </div>
 </nav>
 
-<div class="container" id="content">
-    <script>
-        load_home();
-    </script>
-</div>
+<div class="container" id="content"></div>
 
 </body>
 </html>
